8500f118a4aa3f15b9cad81d7f70efa857820b25
MaximP1henka <87613293+MaximP1henka@users.noreply.github.com>
2023-04-09 20:24:08 +0700

    add save and read to file

--- app/src/main/java/com/laironlf/kitchen_master/DB/UserProducts.java
+++ app/src/main/java/com/laironlf/kitchen_master/DB/UserProducts.java
@@ -1,9 +1,16 @@
 package com.laironlf.kitchen_master.DB;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,9 +19,13 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class UserProducts {
+    static final String TAG = "UserProducts";
     public static String fileName = "appdata";
     public static ArrayList<Product> userProducts = new ArrayList<>();
-
+    public static Context mContext;
+    public void setContext(Context context){
+        mContext = context;
+    }
     public static void capacity(int capacity){
         userProducts.ensureCapacity(capacity);
     }
@@ -35,35 +46,60 @@ public class UserProducts {
         res.delete(res.length()-1,res.length());
         return res.toString();
     }
-
+    private static String stringForWrite(){
+        StringBuilder res = new StringBuilder();
+        for (Product i : userProducts) {
+            res.append(i.productID).append(":").append(i.name).append("|");
+        }
+        res.delete(res.length()-1,res.length());
+        return res.toString();
+    }
     public static void writeData(){
+        FileOutputStream fos = null;
         try {
-            // отрываем поток для записи
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-                    openFileOutput(fileName, MODE_PRIVATE)));
-            // пишем данные
-            bw.write("Содержимое файла");
-            // закрываем поток
-            bw.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            String text = stringForWrite();
+            fos = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+        }
+        catch(IOException ex) {
+            Log.e(TAG, "writeData: \n"+ex);
+        }
+        finally{
+            try{
+                if(fos!=null)
+                    fos.close();
+            }
+            catch(IOException ex){
+                Log.e(TAG, "writeData: \n"+ex);
+            }
         }
     }
     public static void readData(){
+        FileInputStream fin = null;
         try {
-            // открываем поток для чтения
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    openFileInput(fileName)));
-            String str = "";
-            // читаем содержимое
-            while ((str = br.readLine()) != null) {
+            fin = mContext.openFileInput(fileName);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            String[] t = text.split("/|");
+            for (String i : t) {
+                String[] g = i.split(":");
+                int productID = Integer.parseInt(g[0]);
+                String name = g[1];
+                Product temp = new Product(productID, name);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        catch(IOException ex) {
+            Log.e(TAG, "readData: \n"+ex);
+        }
+        finally{
+            try{
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+                Log.e(TAG, "readData: \n"+ex);
+            }
         }
     }
 }
