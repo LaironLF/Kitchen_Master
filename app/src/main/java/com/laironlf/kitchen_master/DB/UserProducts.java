20103f6d595c7653d0219c94649a0c97eff16839
MaximP1henka <87613293+MaximP1henka@users.noreply.github.com>
2023-04-09 18:10:40 +0700

    1

--- app/src/main/java/com/laironlf/kitchen_master/DB/UserProducts.java
+++ app/src/main/java/com/laironlf/kitchen_master/DB/UserProducts.java
@@ -1,8 +1,18 @@
 package com.laironlf.kitchen_master.DB;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class UserProducts {
+    public static String fileName = "appdata";
     public static ArrayList<Product> userProducts = new ArrayList<>();
 
     public static void capacity(int capacity){
@@ -24,5 +34,36 @@ public class UserProducts {
         }
         res.delete(res.length()-1,res.length());
         return res.toString();
+    }
+
+    public static void writeData(){
+        try {
+            // отрываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput(fileName, MODE_PRIVATE)));
+            // пишем данные
+            bw.write("Содержимое файла");
+            // закрываем поток
+            bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void readData(){
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput(fileName)));
+            String str = "";
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
