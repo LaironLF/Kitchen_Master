5d94e1dd7406c48f3ea514f409cc8948cdbedaf9
LaironLF <laironlf@gmail.com>
2023-04-09 22:33:16 +0700

    fix image in recepts RecyclerView

--- app/src/main/java/com/laironlf/kitchen_master/DB/test.java
+++ app/src/main/java/com/laironlf/kitchen_master/DB/test.java
@@ -2,25 +2,56 @@ package com.laironlf.kitchen_master.DB;
 
 public class test {
     public void start() {
+//        DB database = new DB();
 
-//        UserProducts.add(DB.products.get(0));
-//        UserProducts.add(DB.products.get(3));
-//        UserProducts.add(DB.products.get(4));
-//        UserProducts.add(DB.products.get(52));
-//        UserProducts.add(DB.products.get(67));
-//        UserProducts.add(DB.products.get(69));
-//        UserProducts.add(DB.products.get(80));
-//        UserProducts.add(DB.products.get(81));
-//        UserProducts.add(DB.products.get(82));
-//        UserProducts.add(DB.products.get(83));
-//        UserProducts.add(DB.products.get(84));
-//        UserProducts.add(DB.products.get(89));
-//
-//        UserProducts.writeData();
-//        UserProducts.readData();
-//        for (Product i : UserProducts.userProducts) {
-//            i.pr();
+
+        DB.connectionToDataBase.start();
+        try {
+            DB.connectionToDataBase.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+//        DB.query.setSql("SELECT * FROM Products");
+//        DB.query.start();
+//        try {
+//            DB.query.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
 //        }
+
+
+        DB.getProducts.start();
+
+        try {
+            DB.getProducts.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Product i : DB.products) {
+            i.pr();
+        }
+
+
+        UserProducts.add(DB.products.get(0));
+        UserProducts.add(DB.products.get(3));
+        UserProducts.add(DB.products.get(4));
+        UserProducts.add(DB.products.get(52));
+        UserProducts.add(DB.products.get(67));
+        UserProducts.add(DB.products.get(69));
+        UserProducts.add(DB.products.get(80));
+        UserProducts.add(DB.products.get(81));
+        UserProducts.add(DB.products.get(82));
+        UserProducts.add(DB.products.get(83));
+        UserProducts.add(DB.products.get(84));
+        UserProducts.add(DB.products.get(89));
+
+        UserProducts.writeData();
+        UserProducts.readData();
+        for (Product i : UserProducts.userProducts) {
+            i.pr();
+        }
 //        DB.getRecipe.setSettings(UserProducts.getString());
 //        DB.getRecipe.start();
 //        try {
