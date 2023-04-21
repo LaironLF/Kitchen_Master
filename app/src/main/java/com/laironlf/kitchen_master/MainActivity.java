3359f6652e6628465c9aa8d5e735fc928bbc782e
LaironLF <laironlf@gmail.com>
2023-04-22 03:02:10 +0700

    menu design without transition between fragments

--- app/src/main/java/com/laironlf/kitchen_master/MainActivity.java
+++ app/src/main/java/com/laironlf/kitchen_master/MainActivity.java
@@ -7,33 +7,32 @@
 
 package com.laironlf.kitchen_master;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ImageView;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.lifecycle.ViewModel;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
-import com.bumptech.glide.Glide;
-import com.google.android.material.navigation.NavigationView;
 import com.laironlf.kitchen_master.DB.DB;
 import com.laironlf.kitchen_master.DB.UserProducts;
-import com.laironlf.kitchen_master.DB.test;
 import com.laironlf.kitchen_master.databinding.ActivityMainNavBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainNavBinding binding;
+    DrawerLayout drawer;
 
     @NonNull
     @Override
@@ -42,10 +41,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainNavBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
+        Toolbar toolbar = binding.appBarMain.toolbar;
+        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        drawer = binding.drawerLayout;
+        RelativeLayout navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -56,11 +56,13 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+//        NavigationUI.setupWithNavController(navigationView, navController);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-//        test t = new test();
-//        t.start();
     }
 
     @Override
@@ -70,7 +72,19 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                drawer.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
